20fd19a69958d6e2e5ea788e36952afc1b13ba8e
RSarmiento <raphael_kristian@hotmail.com>
2017-11-28 16:51:20 -0500

    move swimlanes left/right, some of story functionality

--- aw-portal/frontend/src/main/frontend/src/app/Swimlane/swimlane.component.ts
+++ aw-portal/frontend/src/main/frontend/src/app/Swimlane/swimlane.component.ts
@@ -14,6 +14,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SwimlaneComponent implements OnInit {
   @Input() board: AwBoard;
   @Input() swimlane: AwSwimlane;
+  sl2: AwSwimlane; //swimlane to be displaced when moving a swimlane around
+  swapSl: AwSwimlane; //for swapping
 
   constructor(private sls: SwimlaneService,
               private sts: StoryService,
@@ -26,9 +28,46 @@ export class SwimlaneComponent implements OnInit {
     });
   }
 
-  moveLeft() {}
+  updateSwimlane(updatedName: string) {
+    console.log("UPDATE SWIMLANE")
+    console.log(updatedName);
+    this.swimlane.name = updatedName;
+    this.board.swimlanes[this.swimlane.order] = this.swimlane;
+    this.sls.updateSwimlane(this.swimlane).subscribe();
+  }
 
-  moveRight() {}
+  moveLeft() {
+    console.log("MOVE LEFT");
+    var ogOrder = this.swimlane.order;
+    this.swimlane.order = ogOrder-1;
+    this.sl2 = this.board.swimlanes[this.swimlane.order];
+    this.sl2.order = ogOrder;
+    this.swapSl = this.sl2;
+    this.board.swimlanes[ogOrder] = this.swapSl;
+    this.board.swimlanes[ogOrder-1] = this.swimlane;
+    console.log(this.sl2);
+    console.log(this.swimlane);
+    console.log(this.board.swimlanes);
+    this.sls.updateSwimlane(this.swimlane).subscribe();
+    this.sls.updateSwimlane(this.sl2).subscribe();
+  }
+
+  moveRight() {
+    console.log("MOVE RIGHT");
+    var ogOrder = this.swimlane.order;
+    this.swimlane.order = ogOrder+1;
+    this.sl2 = this.board.swimlanes[this.swimlane.order];
+    this.sl2.order = ogOrder;
+    this.swapSl = this.sl2;
+    this.board.swimlanes[ogOrder] = this.swapSl;
+    this.board.swimlanes[ogOrder+1] = this.swimlane;
+    console.log(this.sl2);
+    console.log(this.swimlane);
+    console.log(this.board.swimlanes);
+    this.sls.updateSwimlane(this.swimlane).subscribe();
+    this.sls.updateSwimlane(this.sl2).subscribe();
+    
+  }
 
   delete(sl: AwSwimlane): void {
     this.sls.deleteSwimlane(sl).subscribe((b: Boolean) => {
@@ -44,5 +83,21 @@ export class SwimlaneComponent implements OnInit {
 
     });
   }
+
+  createStory(title: string, slId: number, desc: string, points: number) {
+    console.log("CREATE STORY");
+    // sl.order = (this.bu.swimlanes) ? this.bu.swimlanes.length : 0;
+    // console.log(sl);
+    // this.sls.createSwimlane(sl).subscribe((result: AwSwimlane) => {
+    //     this.bu.swimlanes.push(result);
+    //     this.sl = {
+    //         id: 0,
+    //         boardId: this.bu.id,
+    //         name: "",
+    //         order: 0,
+    //         stories: []
+    //     };
+    // });
+}
 
 }
