099f9b1daf79716a711b52e66552c8234605119c
Sam Wood <solemankirby@gmail.com>
2017-11-29 14:52:51 -0500

    Small change to how CORS is handled on Zuul; no issues, assuming CORS is handled on each end service

--- aw-zuul/src/main/java/com/revature/aw/zuul/AwZuulApplication.java
+++ aw-zuul/src/main/java/com/revature/aw/zuul/AwZuulApplication.java
@@ -19,22 +19,4 @@ public class AwZuulApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AwZuulApplication.class, args);
 	}
-	
-//	@Bean
-//	public CorsFilter corsFilter() {
-//	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//	    final CorsConfiguration config = new CorsConfiguration();
-//	    config.setAllowCredentials(true);
-//	    config.addAllowedOrigin("*");
-//	    config.addAllowedHeader("*");
-//	    config.addAllowedMethod("OPTIONS");
-//	    config.addAllowedMethod("HEAD");
-//	    config.addAllowedMethod("GET");
-//	    config.addAllowedMethod("PUT");
-//	    config.addAllowedMethod("POST");
-//	    config.addAllowedMethod("DELETE");
-//	    config.addAllowedMethod("PATCH");
-//	    source.registerCorsConfiguration("/**", config);
-//	    return new CorsFilter(source);
-//	}
 }
