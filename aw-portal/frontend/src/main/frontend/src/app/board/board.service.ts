d2a75a0cb2bd510cb2eae0d5abe3769cc79be252
Sam Wood <solemankirby@gmail.com>
2017-11-30 13:44:01 -0500

    Added post generic method to backend service

--- aw-portal/frontend/src/main/frontend/src/app/board/board.service.ts
+++ aw-portal/frontend/src/main/frontend/src/app/board/board.service.ts
@@ -2,29 +2,11 @@ import { BackendService } from './../backend.service';
 import { AwBoard } from './../domain/aw-board';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of';
-import { Headers, Http, RequestOptions } from '@angular/http';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Base64 } from 'js-base64';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/take';
-import 'rxjs/add/operator/retryWhen';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class BoardService {
-  url: string;
-  headers: Headers;
-  options: RequestOptions;
-  creds: String;
-  updatedUser: string;
-  zuulUrl: string = "http://localhost:8765";
 
-  constructor(private http: Http, private router: Router, private backend: BackendService) {}
+  constructor(private backend: BackendService) {}
 
   getBoard(id: number): Observable<AwBoard> {
     return this.backend.get<AwBoard>("/aw_boards/getBoard/" + id);
