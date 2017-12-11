0cb966e9c018c7e7460fd5e79f788f16df79ee22
RSarmiento <raphael_kristian@hotmail.com>
2017-12-10 20:27:27 -0500

    current

--- aw_BoardValidation/src/main/java/com/revature/aw/services/BoardUserRoleServices.java
+++ aw_BoardValidation/src/main/java/com/revature/aw/services/BoardUserRoleServices.java
@@ -15,23 +15,46 @@ public class BoardUserRoleServices
 {
 	@Autowired
 	private Dao dao;
-	
+	/**
+	 * Service method to find a list of boards a User can view.
+	 * @param userId The id of the User whose boards you want.
+	 * @return A List of BoardUserRoles, essentially a list of Boards the User can view.
+	 */
 	public List<BoardUserRole> findByUserId(int userId) {
 		return dao.findByUserId(userId);
 	}
 	
+	/**
+	 * Service method to save a new BoardUserRole. Used to add a new User to a Board.
+	 * @param bur The BoardUserRole to be added.
+	 * @return The saved BoardUserRole.
+	 */
 	public BoardUserRole save(BoardUserRole bur) {
 		return dao.save(bur);
 	}
 	
+	/**
+	 * Service method to delete a User from a Board.
+	 * @param bur A BUR object with the user and board to be deleted from.
+	 */
 	public void delete(BoardUserRole bur) {
 		dao.delete(bur);
 	}
 	
+	/**
+	 * Service method to delete all the Users attached to a board.
+	 * @param boardId Board to be deleted.
+	 */
 	public void deleteByBoardId(int boardId) {
 		dao.deleteByBoardId(boardId);
 	}
 	
+	/**
+	 * Service method to determine if a User can access a specific Board.
+	 * @param userId The User to check.
+	 * @param boardId The Board to check on.
+	 * @return True if the User can access the Board. False if they can't.
+	 */
 	public boolean determinePrivileges(int userId, int boardId) {
 		boolean canView = false;
 		BoardUserRole bur = new BoardUserRole(0,0,0);
@@ -42,6 +65,12 @@ public class BoardUserRoleServices
 		return canView;
 	}
 	
+	/**
+	 * Service method to determine if a User can access a specific Board.
+	 * @param userId The User to check.
+	 * @param boardId The Board to check on.
+	 * @return A BUR object if valid, null if not.	
+	 */
 	public BoardUserRole findByUserIdAndBoardId(int userId,int boardId) {
 		return dao.findByUserIdAndBoardId(userId, boardId);
 	}
